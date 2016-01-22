bcf2e871efbc2a6260ecc0827c2b98ed16e1fb17
talluriabhishek <svt244@nyu.edu>
2016-01-21 23:23:34 -0500

    adding dynamic content

--- shopping/views.py
+++ shopping/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def item_list(request):
-    return render(request, 'shopping/item_list.html', {})
+    items = Item.objects.all()
+    return render(request, 'shopping/item_list.html', {"items":items})
